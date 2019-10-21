3d05b6cba97687477edff1848e5c8fc6eed54ab1
cheng <732345907@qq.com>
2019-10-21 20:52:58 +0800

    add

---
+++ application/home/controller/homes/Homes.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\home\controller\homes;
+
+use think\Config;
+use think\Controller;
+use app\home\service\homesservice;
+
+class Homes extends Controller
+{
+    public function index(){
+
+        if($this->request->isGet()){
+
+        }
+
+    }
+
+}
