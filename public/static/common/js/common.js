1b367a8b48b6b5d29a67636ecdd63af06627f225
‘jason <syl_880921@163.com>
2019-11-07 11:46:46 +0800

    修改76

--- public/static/common/js/common.js
+++ public/static/common/js/common.js
@@ -2,13 +2,14 @@ var index_module = (function (){
     //退出登录
     var user_logout = function user_logout(objthis){
         var url = $(objthis).attr('data-url');
+        var url2 = $(objthis).attr('location_url');
         $.post(
             url,
             '',
             function (ret){
                 if(ret.status == true){
                     layer.msg(ret.message,{icon:1,time:1000},function (){
-                        parent.location.reload();
+                        location.href = url2;
                     });
                 }else{
                     layer.msg(ret.message,{icon:2,time:1500});
