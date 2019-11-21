688858ca1c531d4be410f35dba8d0818daecf1f6
‘jason <syl_880921@163.com>
2019-11-21 17:51:40 +0800

    修改77

--- public/static/common/js/public.js
+++ public/static/common/js/public.js
@@ -40,3 +40,13 @@ $(function (){
         $('#mobile_phone').html(newPhone);
     }
 });
+
+
+
+function login_btn(objthis){
+    var login_url2 = $(objthis).attr('login_url');
+    var loca_url2 = $(objthis).attr('loca_url');
+    var loca_url = encodeURIComponent(loca_url2);
+    var login_url = login_url2+'?artId='+loca_url;
+    location.href = login_url;
+}
