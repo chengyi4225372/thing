6765a01fe328e9ade03ece91a41e14086ba2a101
‘jason <syl_880921@163.com>
2019-12-04 11:38:15 +0800

    提交

--- application/extra/curl.php
+++ application/extra/curl.php
@@ -6,8 +6,8 @@ return [
 //    'website' => 'http://172.26.3.13:8008/index.php',//官网
     'website' => 'http://www.hqyonline.com/',//官网
 
-    'hzs' => 'http://172.26.3.13:8010/index.php',//会找事
-//    'hzs' => 'http://www.hqyonline.com',//会找事
+//    'hzs' => 'http://172.26.3.13:8010/index.php',//会找事
+    'hzs' => 'http://www.35serve.com',//会找事
 
 //    'hys' => 'http://172.26.3.13:8011/index.php',//惠优税
     'hys' => 'http://admin.hqyonline.com/',//惠优税
