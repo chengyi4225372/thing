38c420c30f8cad5b40f8bc844fd92c02f972044a
‘jason <syl_880921@163.com>
2019-12-05 18:02:31 +0800

    提交

--- application/v1/service/Workservice.php
+++ application/v1/service/Workservice.php
@@ -1,6 +1,9 @@
 <?php
 namespace app\v1\service;
-use app\common\model\Work;;
+
+use app\common\model\Work;
+
+;
 use app\common\model\News;
 use app\common\model\Cases;
 use app\common\model\Solution;
@@ -12,6 +15,7 @@ class Workservice
 {
     // 静态对象
     protected static $instance = null;
+
     /**
      * @DESC：单例
      * @return null|static
@@ -29,31 +33,58 @@ class Workservice
     /**
      * 前台获取最高的3条信息
      */
-    public function three(){
-        $where = ['del_time'=>0];
-        $data = Work::instance()->where($where)->order(['sort'=>'desc','create_time'=>'desc'])->limit(3)->select();
+    public function three()
+    {
+        $where = [];
+        $where['status'] = 1;
+        $data = Work::instance()->where($where)->order('sort desc,create_time')->limit(3)->select();
         return $data;
     }
 
 
     /**
-     * title string
-     *获取资讯列表
+     * @DESC：获取资讯列表
+     * @param $params
+     * @return mixed
+     * @author: jason
+     * @date: 2019-12-05 05:42:31
      */
-    public function getNewList($title){
-        if(empty($title) || !isset($title)){
+    public function getNewList($params)
+    {
+        $where = [];
+        //按字段类型搜索
+        if (!empty($params['searchField']) && !empty($params['searchValue'])) {
+            $searchValue = preg_replace("/(\n)|(\s)|(\t)|(\')|(')|(，)/", ',', trim($params['searchValue']));
+            $searchValue = explode(',', $searchValue);
 
-            $where = ['del_time'=>0];
-        }else {
-
-            $where =[
-                'del_time'=>0,
-                'title|keyword'=>['like','%'.$title.'%'],
-            ];
+            $searchValue = array_filter($searchValue, function ($par) {
+                return !empty($par);
+            });
+            switch ($params['searchField']) {
+                case 1:
+                    $good = array_map(function ($param) {
+                        return '%' . $param . '%';
+                    }, $searchValue);
+                    $where['title'] = array('like', $good, 'or');
+                    break;
+                case 2:
+                    $good = array_map(function ($param) {
+                        return '%' . $param . '%';
+                    }, $searchValue);
+                    $where['keyword'] = array('like', $good, 'or');
+                    break;
+                case 3:
+                    $good = array_map(function ($param) {
+                        return '%' . $param . '%';
+                    }, $searchValue);
+                    $where['desc'] = array('like', $good, 'or');
+                    break;
+            }
         }
+        $where['status'] = 1;
 
-        $list  = Work::instance()->where($where)->order(['sort'=>'desc','create_time'=>'desc'])->paginate(15);
-        return  $list;
+        $list = Work::instance()->where($where)->order('sort desc,create_time')->paginate(15);
+        return $list;
     }
 
 
@@ -62,7 +93,8 @@ class Workservice
      *
      * 添加 数据
      */
-    public function setAddArray($array){
+    public function setAddArray($array)
+    {
         $ret = Work::instance()->data($array)->save();
         return $ret;
     }
@@ -74,18 +106,19 @@ class Workservice
      * id    string
      *return bool
      */
-    public function  updateByArray($array,$id){
-        $w = ['id'=>$id];
+    public function updateByArray($array, $id)
+    {
+        $w = ['id' => $id];
 
-        if(empty($id) || $id <=0){
+        if (empty($id) || $id <= 0) {
             return false;
         }
 
-        if(empty($array)){
-            return  false;
+        if (empty($array)) {
+            return false;
         }
 
-        $ret =  Work::instance()->where($w)->update($array);
+        $ret = Work::instance()->where($w)->update($array);
         return $ret;
     }
 
@@ -93,12 +126,13 @@ class Workservice
     /**
      * 通过id 获取信息
      */
-    public function getIdInfo($id){
+    public function getIdInfo($id)
+    {
 
-        if(empty($id) || $id <=0){
+        if (empty($id) || $id <= 0) {
             return false;
         }
-        $w    = ['id'=>$id];
+        $w = ['id' => $id];
         $info = Work::instance()->where($w)->find();
         return $info;
     }
@@ -108,13 +142,14 @@ class Workservice
      * id string
      * return bool
      */
-    public function setDel($id){
-        if(empty($id) || $id <=0){
+    public function setDel($id)
+    {
+        if (empty($id) || $id <= 0) {
             return false;
         }
 
-        $w   = ['id'=>$id];
-        $c   = ['del_time'=>time()];
+        $w = ['id' => $id];
+        $c = ['status' => 2];
         $ret = Work::instance()->where($w)->update($c);
         return $ret;
     }
@@ -125,23 +160,24 @@ class Workservice
      * sort string|int
      * return bool
      */
-    public  function toSetsort($id,$sort){
-        if(empty($id) || !isset($id)){
+    public function toSetsort($id, $sort)
+    {
+        if (empty($id) || !isset($id)) {
             return false;
         }
 
-        if(empty($sort) || $sort <= 0){
+        if (empty($sort) || $sort <= 0) {
             $sort = 0;
         }
 
-        $w   = ['id'=>$id];
-        $arr = ['sort'=>$sort];
+        $w = ['id' => $id];
+        $arr = ['sort' => $sort];
 
         $ret = Work::instance()->where($w)->update($arr);
 
-        if($ret !== false){
+        if ($ret !== false) {
             return true;
-        }else{
+        } else {
             return false;
         }
 
@@ -153,21 +189,22 @@ class Workservice
      * id string
      * return array|null
      */
-    public function getTop($id){
-        if(empty($id) || !isset($id)){
+    public function getTop($id)
+    {
+        if (empty($id) || !isset($id)) {
             return false;
         }
         $where = [
-            'id'=>['<',$id],
-            'del_time'=>0
+            'id' => ['<', $id],
+            'status' => 1
         ];
 
-        $info = Work::instance()->where($where)->order(['sort'=>'desc','create_time'=>'desc'])->find();
+        $info = Work::instance()->where($where)->order('sort desc,create_time')->find();
 
-        if(empty($info)){
-            return  $info ='';
-        }else{
-            return  $info;
+        if (empty($info)) {
+            return $info = '';
+        } else {
+            return $info;
         }
 
     }
@@ -177,21 +214,22 @@ class Workservice
      * id string
      * return array|null
      */
-    public function getNext($id){
-        if(empty($id) || !isset($id)){
+    public function getNext($id)
+    {
+        if (empty($id) || !isset($id)) {
             return false;
         }
 
         $where = [
-            'id'=>['>',$id],
-            'del_time'=>0
+            'id' => ['>', $id],
+            'status' => 1
         ];
-        $info = Work::instance()->where($where)->order(['sort'=>'asc','create_time'=>'desc'])->find();
+        $info = Work::instance()->where($where)->order('sort desc,create_time desc')->find();
 
-        if(empty($info)){
-            return  $info ='';
-        }else{
-            return  $info;
+        if (empty($info)) {
+            return $info = '';
+        } else {
+            return $info;
         }
     }
 
@@ -200,20 +238,21 @@ class Workservice
      *
      * return string|int
      */
-    public function getCount($title){
-        if(empty($title) || !isset($title)){
+    public function getCount($title)
+    {
+        if (empty($title) || !isset($title)) {
 
-            $where = ['del_time'=>0];
-        }else {
+            $where = ['status' => 1];
+        } else {
 
-            $where =[
-                'del_time'=>0,
-                'title|keyword'=>['like','%'.$title.'%'],
+            $where = [
+                'status' => 1,
+                'title|keyword' => ['like', '%' . $title . '%'],
             ];
         }
 
-        $count  = Work::instance()->where($where)->order(['sort'=>'desc','create_time'=>'desc'])->count('id');
-        return  $count;
+        $count = Work::instance()->where($where)->order('sort desc,create_time desc')->count('id');
+        return $count;
     }
 
 
@@ -223,35 +262,37 @@ class Workservice
      * page  string | int
      * return array|null
      */
-    public function  Getinfolist($title,$page){
-        if(empty($title) || !isset($title)){
+    public function Getinfolist($title, $page)
+    {
+        if (empty($title) || !isset($title)) {
 
-            $where = ['del_time'=>0];
-        }else {
+            $where = ['status' => 1];
+        } else {
 
-            $where =[
-                'del_time'=>0,
-                'title|keyword'=>['like','%'.$title.'%'],
+            $where = [
+                'status' => 1,
+                'title|keyword' => ['like', '%' . $title . '%'],
             ];
         }
 
         $next = 10;
-        if(empty($page)){
+        if (empty($page)) {
             $page = 0;
-        }else {
-            $page= $next * $page;
+        } else {
+            $page = $next * $page;
         }
 
-        $list  = Work::instance()->where($where)->order(['sort'=>'desc','create_time'=>'desc'])->limit($page,$next)->select();
-        return  $list;
+        $list = Work::instance()->where($where)->order('sort desc,create_time desc')->limit($page, $next)->select();
+        return $list;
     }
 
 
     /**
      * 获取有效数据总条数
      */
-    public function getWorkCount(){
-        $count = Work::instance()->where(['del_time'=>0])->count();
+    public function getWorkCount()
+    {
+        $count = Work::instance()->where(['status' => 1])->count();
         return $count;
     }
 
@@ -273,24 +314,24 @@ class Workservice
         //分页起始值
         $start = $page_size * ($current_page - 1);
 
-        if(isset($params['title']) && !empty($params['title'])){
+        if (isset($params['title']) && !empty($params['title'])) {
             //将中文逗号替换成英文逗号
-            if(strpos($params['title'],'，')){
-                $params['title'] = str_replace('，',',',$params['title']);
+            if (strpos($params['title'], '，')) {
+                $params['title'] = str_replace('，', ',', $params['title']);
             }
             //将空格替换成英文逗号
-            $params['title'] = preg_replace('/\s{1,}/',' ',$params['title']);
-            $params['title'] = str_replace(' ',',',$params['title']);
-            $params['title'] = explode(',',$params['title']);
+            $params['title'] = preg_replace('/\s{1,}/', ' ', $params['title']);
+            $params['title'] = str_replace(' ', ',', $params['title']);
+            $params['title'] = explode(',', $params['title']);
 
-            $params['title'] = array_filter($params['title'],function ($params){
+            $params['title'] = array_filter($params['title'], function ($params) {
                 return !empty($params);
             });
-            $caseArr = array_map(function($par){
-                return '%'.$par.'%';
-            },$params['title']);
+            $caseArr = array_map(function ($par) {
+                return '%' . $par . '%';
+            }, $params['title']);
 
-            $where['title'] = ['LIKE',$caseArr,'OR'];
+            $where['title'] = ['LIKE', $caseArr, 'OR'];
         }
         $where['status'] = 1;
         //分页url参数
@@ -298,7 +339,7 @@ class Workservice
             'query' => request()->param(),
         ];
         $caseInfo = Cases::instance()->where($where)
-            ->limit($start,$page_size)
+            ->limit($start, $page_size)
             ->order('id', 'desc')
             ->paginate($page_size, false, $config);
         $page = $caseInfo->render();
@@ -318,7 +359,7 @@ class Workservice
     public function getOneData($params)
     {
 
-        if(empty($params)){
+        if (empty($params)) {
             return false;
         }
         $where = [];
@@ -344,7 +385,7 @@ class Workservice
         $add['add_time'] = time();
         $add['update_time'] = time();
         $res = Cases::instance()->insert($add);
-        if($res === false){
+        if ($res === false) {
             return false;
         }
         return true;
@@ -364,7 +405,7 @@ class Workservice
         $add['describe'] = $params['describe'];
         $add['update_time'] = time();
         $res = Cases::instance()->where(['id' => $params['id']])->update($add);
-        if($res === false){
+        if ($res === false) {
             return false;
         }
         return true;
@@ -378,7 +419,7 @@ class Workservice
     public function delcase($params)
     {
         $res = Cases::instance()->where(['id' => $params['id']])->update(['status' => 2]);
-        if($res === false){
+        if ($res === false) {
             return false;
         }
         return true;
@@ -400,24 +441,24 @@ class Workservice
         //分页起始值
         $start = $page_size * ($current_page - 1);
 
-        if(isset($params['title']) && !empty($params['title'])){
+        if (isset($params['title']) && !empty($params['title'])) {
             //将中文逗号替换成英文逗号
-            if(strpos($params['title'],'，')){
-                $params['title'] = str_replace('，',',',$params['title']);
+            if (strpos($params['title'], '，')) {
+                $params['title'] = str_replace('，', ',', $params['title']);
             }
             //将空格替换成英文逗号
-            $params['title'] = preg_replace('/\s{1,}/',' ',$params['title']);
-            $params['title'] = str_replace(' ',',',$params['title']);
-            $params['title'] = explode(',',$params['title']);
+            $params['title'] = preg_replace('/\s{1,}/', ' ', $params['title']);
+            $params['title'] = str_replace(' ', ',', $params['title']);
+            $params['title'] = explode(',', $params['title']);
 
-            $params['title'] = array_filter($params['title'],function ($params){
+            $params['title'] = array_filter($params['title'], function ($params) {
                 return !empty($params);
             });
-            $caseArr = array_map(function($par){
-                return '%'.$par.'%';
-            },$params['title']);
+            $caseArr = array_map(function ($par) {
+                return '%' . $par . '%';
+            }, $params['title']);
 
-            $where['title'] = ['LIKE',$caseArr,'OR'];
+            $where['title'] = ['LIKE', $caseArr, 'OR'];
         }
         $where['status'] = 1;
         //分页url参数
@@ -425,7 +466,7 @@ class Workservice
             'query' => request()->param(),
         ];
         $solutionInfo = Solution::instance()->where($where)
-            ->limit($start,$page_size)
+            ->limit($start, $page_size)
             ->order('id', 'desc')
             ->paginate($page_size, false, $config);
         $page = $solutionInfo->render();
@@ -445,7 +486,7 @@ class Workservice
      */
     public function addsolution($params)
     {
-        if(empty($params)){
+        if (empty($params)) {
             return false;
         }
         $add = [];
@@ -456,7 +497,7 @@ class Workservice
         $add['add_time'] = time();
         $add['add_user'] = Cookie('username');
         $res = Solution::instance()->insert($add);
-        if($res === false){
+        if ($res === false) {
             return false;
         }
         return true;
@@ -471,10 +512,10 @@ class Workservice
      */
     public function editsolution($params)
     {
-        if(empty($params)){
+        if (empty($params)) {
             return false;
         }
-        if(empty($params['id']) || !isset($params['id'])){
+        if (empty($params['id']) || !isset($params['id'])) {
             return false;
         }
         $add = [];
@@ -484,7 +525,7 @@ class Workservice
         $where = [];
         $where['id'] = $params['id'];
         $res = Solution::instance()->where($where)->update($add);
-        if($res === false){
+        if ($res === false) {
             return false;
         }
         return true;
@@ -499,7 +540,7 @@ class Workservice
      */
     public function getOneSolution($params)
     {
-        if(empty($params)){
+        if (empty($params)) {
             return false;
         }
         $where = [];
@@ -512,7 +553,7 @@ class Workservice
     public function delsolution($params)
     {
         $res = Solution::instance()->where(['id' => $params['id']])->update(['status' => 2]);
-        if($res === false){
+        if ($res === false) {
             return false;
         }
         return true;
@@ -540,7 +581,7 @@ class Workservice
      */
     public function getcasedetail($params)
     {
-        if(empty($params['id']) || !isset($params['id'])){
+        if (empty($params['id']) || !isset($params['id'])) {
             return false;
         }
         $id = $params['id'];
