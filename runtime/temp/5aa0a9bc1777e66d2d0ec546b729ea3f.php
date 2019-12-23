0de1e5a473ec1a6c6866605bdd46acd0f455dafc
cheng <732345907@qq.com>
2019-12-23 17:01:34 +0800

    add

--- runtime/temp/5aa0a9bc1777e66d2d0ec546b729ea3f.php
+++ runtime/temp/5aa0a9bc1777e66d2d0ec546b729ea3f.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:6:{s:116:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\thing\public/../application/home\view\index\productservice.html";i:1577069019;s:97:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\thing\application\home\view\common\logo.html";i:1576806779;s:98:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\thing\application\home\view\common\login.html";i:1576806779;s:99:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\thing\application\home\view\common\footer.html";i:1576848615;s:97:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\thing\application\home\view\common\left.html";i:1577068758;s:98:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\thing\application\home\view\common\alert.html";i:1577068879;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:6:{s:116:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\thing\public/../application/home\view\index\productservice.html";i:1577069833;s:97:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\thing\application\home\view\common\logo.html";i:1576806779;s:98:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\thing\application\home\view\common\login.html";i:1576806779;s:99:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\thing\application\home\view\common\footer.html";i:1577090558;s:97:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\thing\application\home\view\common\left.html";i:1577090558;s:98:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\thing\application\home\view\common\alert.html";i:1577069294;}*/ ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="/static/spirit/font/syht.css">
     <link rel="stylesheet" href="/static/spirit/css/product _service.css">
     <link rel="stylesheet" href="/static/spirit/css/footer.css">
+    <link rel="stylesheet" href="/static/spirit/css/alert.css">
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
     <script src="/static/spirit/js/clamp.js"></script>
     <script src="/static/assets/plugins/layui/layui.all.js"></script>
@@ -351,7 +352,7 @@
                 <dl>
                     <dt>客服热线</dt>
                     <dd><a href="javascript:;">400-150-9896</a></dd>
-                    <dd><a href="javascript:;">18186194461</a></dd>
+                    <dd><a href="javascript:;">181-8619-4461</a></dd>
                 </dl>
                 <dl>
                     <dt>办公地址</dt>
@@ -413,7 +414,7 @@
             </div>
             <div>
                 <p>获取税筹方案</p>
-                <p>400-150-9898</p>
+                <p>400-150-9896</p>
             </div>
         </div>
     </div>
@@ -425,7 +426,7 @@
 </div>
 
         <!-- 弹窗 -->
-        <!-- 弹框 -->
+        
 <div class="pop-up-box" id="popbox">
     <div class="form">
         <div class="form-titile">
