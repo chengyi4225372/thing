fc02f9337322bc519329f9eb1257260ccc1709cb
‘jason <syl_880921@163.com>
2020-01-17 10:54:42 +0800

    提交

--- application/extra/curl.php
+++ application/extra/curl.php
@@ -1,18 +1,18 @@
 <?php
 return [
     //    'hlg' => 'http://172.26.3.13:8009/index.php',//惠灵工
-    'hlg' => 'http://hui0g.hui7y.com',//惠灵工
+    'hlg' => 'https://hui0g.hui7y.com',//惠灵工
 
     //    'website' => 'http://172.26.3.13:8008/index.php',//官网
     'website' => 'https://www.hui7y.com', //官网
 
     //    'hzs' => 'http://172.26.3.13:8010/index.php',//会找事
-    'hzs' => 'http://hui0g.hui7y.com', //会找事
+    'hzs' => 'https://job.hui7y.com', //会找事
 
     //    'hys' => 'http://172.26.3.13:8011/index.php',//惠优税
-    'hys' => 'http://hui3y.hui7y.com', //惠优税
+    'hys' => 'https://hui3y.hui7y.com', //惠优税
 
-    'redirect_url' => 'http://hlg_pub.hui7y.com/#', //登录重定向地址
-    'login_url' => 'http://hlg_pub.hui7y.com/#/login', //登录地址
-    'register_url' => 'http://hlg_pub.hui7y.com/#/register', //注册地址
+    'redirect_url' => 'https://hlg_pub.hui7y.com/#', //登录重定向地址
+    'login_url' => 'https://hlg_pub.hui7y.com/#/login', //登录地址
+    'register_url' => 'https://hlg_pub.hui7y.com/#/register', //注册地址
 ];
